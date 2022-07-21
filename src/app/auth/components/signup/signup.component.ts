bc8d86e57a08bfe69ae9b5010f3a5e1342febb45
Robin Haider <robinhaider69@gmail.com>
2022-07-21 08:28:41 +0600

    signup page

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+  loading = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  signup() {
+    this.loading = true;
+    this.router.navigate(['/']);
+  }
+
+  private createForm() {
+
+    this.signupForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', Validators.required),
+    });
+  }
 }
